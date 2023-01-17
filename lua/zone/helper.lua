10539f228d352d2e837f3776d1ae8797a85f2105
Aquib <aquibjavedt007@gmail.com>
2023-01-17 12:40:56 +0530

    converted treadmill base to extmarks

--- lua/zone/helper.lua
+++ lua/zone/helper.lua
@@ -1,87 +1,85 @@
--- TODO: autocmds are a mess: make it clean
+-- TODO: get place on correct (row,col), read about `ephemeral`
 local H = {}
-local is_running, timer
+local ns = vim.api.nvim_create_namespace("zone")
+local is_running, timer, id
 local zone_win, zone_buf
 local uv = vim.loop
+local w = 6
 local helper_opts = {
-    tick_time = 100,
+    tick_time = 75,
     win_opts = {
-		relative="win", width=vim.o.columns, height=vim.o.lines - 2,
-		border="none", row=3, col=0, -- style='minimal'
+		relative="win", width=vim.o.columns-w, height=vim.o.lines - 2,
+		border="none", row=0, col=w, style='minimal'
     }
 }
 
---- Put lines from original buf to the fake_buf
----@param og_buf number
----@return table the lines
 H.set_buf_view = function(og_buf)
-    local start_line = vim.fn.line("w0") - 1
+    local start_line = vim.fn.line("w0")
     local end_line = start_line + vim.o.lines
 
     local local_content = vim.api.nvim_buf_get_lines(og_buf, start_line, end_line, false)
-    vim.api.nvim_buf_set_lines(zone_buf, 0, -1, true, local_content)
 
-    return local_content
+    local matrix = {}
+    for i=0, #local_content-1 do
+        local newt = {}
+        local line = local_content[i+1]
+
+        for j=0, line:len() do
+            local nice = vim.treesitter.get_captures_at_pos(og_buf, i+start_line, j-1)
+            nice = vim.tbl_filter(function(h) return h.capture ~= "spell" end, nice)
+            local hl = #nice > 0 and nice[#nice].capture or 'none'
+            table.insert(newt, {line:sub(j, j+(helper_opts.headache and 1 or 0)), "@"..hl})
+        end
+
+        table.insert(matrix, newt)
+    end
+
+    id = vim.api.nvim_buf_set_extmark(zone_buf, ns, 0, 0, {
+        virt_lines=matrix,
+        virt_lines_leftcol = true,
+    })
+
+    return matrix, ns, id
 end
 
 H.zone_close = function()
 	if is_running then
         vim.schedule(function()
-            if vim.api.nvim_win_is_valid(zone_win) then
-                vim.api.nvim_win_close(zone_win, true)
-            end
-            if vim.api.nvim_buf_is_valid(zone_buf) then
-                vim.api.nvim_buf_delete(zone_buf, {force=true})
-            end
+            pcall(vim.api.nvim_win_close, zone_win, true)
+            vim.api.nvim_buf_del_extmark(zone_buf, ns, id)
+            pcall(vim.api.nvim_buf_delete, zone_buf, {force=true})
+
             if timer:is_active() then timer:stop() end
             if not timer:is_closing() then timer:close() end
+
             if type(H.on_exit) == "function" then H.on_exit() end
             is_running = false
         end)
 	end
 end
 
---- On each tick, run the callback function specified
----@param cb function
----@return number timer
 H.on_each_tick = function(cb)
 	timer = uv.new_timer()
-	uv.timer_start(timer, 1000, helper_opts.tick_time or 100, vim.schedule_wrap(cb))
+	timer:start(1000, helper_opts.tick_time or 100, vim.schedule_wrap(cb))
 
     return timer
 end
 
---- Creates and Initiates the necessary conditions.
----@param on_init function: callback to invoke on zone startup
----@return number: the buf handler
-H.create_and_initiate = function(on_init)
-    helper_opts = vim.tbl_deep_extend("force", helper_opts, H.opts or {})
+H.create_and_initiate = function(on_init, opts)
+    helper_opts = vim.tbl_deep_extend("force", helper_opts, opts or {})
+
     -- TODO: pass bufnr into on_init maybe
     if type(on_init) == "function" then on_init() end
-	local pos_before = vim.api.nvim_win_get_cursor(0)
-	local ft = vim.bo.ft
-
-    local new_row = vim.fn.line('.') - vim.fn.line('w0') + 1
-    local pos_after = {new_row, pos_before[2]}
 
 	zone_buf = vim.api.nvim_create_buf(false, true)
 	zone_win = vim.api.nvim_open_win(zone_buf, false, helper_opts.win_opts)
     is_running = true
 
-    vim.schedule_wrap(function()
-        vim.api.nvim_win_set_cursor(zone_win, pos_after)
-    end)
-
-	vim.api.nvim_buf_set_option(zone_buf, 'filetype', ft)
     vim.api.nvim_win_set_option(zone_win, 'winhl', 'Normal:Normal')
     -- TODO: add this keymap stuff later without breaking anything
     -- vim.keymap.set('n', '<Esc>', function() Internal.zone_close() end, {noremap=true, buffer=zone_buf})
 
-    vim.api.nvim_create_autocmd('CursorMoved', {
-        pattern="*",
-        callback=function() H.zone_close() end,
-        once=true
-    })
+    vim.api.nvim_create_autocmd('CursorMoved', { callback=H.zone_close, once=true })
 
     return zone_buf
 end
--- lua/zone/styles/treadmill.lua
+++ lua/zone/styles/treadmill.lua
@@ -1,48 +1,31 @@
--- TODO: fix the spacing stuff
+-- TODO: smoothen around the rough edges, calculate the column offset, add back opts
 local treadmill = {}
 local mod = require("zone.helper")
 local fake_buf
-local opts_table
+local local_opts = {headache=true}
 
-local line_spaces
-
-local rotate = function()
+local rotate = function(grid, ns, id)
     if not vim.api.nvim_buf_is_valid(fake_buf) then return end
-	local lines = vim.api.nvim_buf_get_lines(fake_buf, 0, -1, true)
-	local new_lines = {}
-	for i, line in ipairs(lines) do
-		local spaces = line_spaces[i]
-		local clean_line = vim.trim(line)
-        local new_line
-        local dir = opts_table and opts_table.direction or "left"
-        if dir == "right" then
-            new_line = spaces .. clean_line:sub(-1, -1) .. clean_line:sub(1, -2)
-        else
-            new_line = spaces .. clean_line:sub(2, -1) .. clean_line:sub(1, 1)
-        end
-		table.insert(new_lines, new_line)
-	end
 
-    vim.schedule(function()
-        vim.api.nvim_buf_set_lines(fake_buf, 0, -1, true, new_lines)
-    end)
+    for i=0, #grid-1 do
+        local item = table.remove(grid[i+1], 1)
+        table.insert(grid[i+1], item)
+    end
+
+    vim.api.nvim_buf_set_extmark(fake_buf, ns, 0, 0, {
+        virt_lines=grid, id=id,
+    })
 end
 
 function treadmill.start(opts)
-    opts_table = opts
+    local_opts = opts
     local before_buf = vim.api.nvim_get_current_buf()
 
-    fake_buf = mod.create_and_initiate(nil)
+    fake_buf = mod.create_and_initiate(function() end, local_opts)
 
-    local zone_lines = mod.set_buf_view(before_buf)
+    local matrix, ns, id = mod.set_buf_view(before_buf)
 
-    line_spaces = {}
-    for _,line in ipairs(zone_lines) do
-        local this_line = line:match("^(%s*)") or ""
-        table.insert(line_spaces, this_line)
-    end
-
-    mod.on_each_tick(rotate)
+    mod.on_each_tick(function() rotate(matrix, ns, id) end)
 end
 
 return treadmill

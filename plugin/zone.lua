8ed5cafc5cd6e502c557bda9c80fa7f3e03013a8
Garrett Oreilly <hey@heygarrett.com>
2023-04-06 17:59:25 -0400

    style(user-command): format with tabstop of 4

--- plugin/zone.lua
+++ plugin/zone.lua
@@ -1,14 +1,13 @@
-
 vim.api.nvim_create_user_command('Zone', function(opt)
     local style = opt.args
     if style == "" then
         style = "treadmill"
     end
-    require("zone.styles."..style).start()
+    require("zone.styles." .. style).start()
 end, {
-        complete=function()
-            return {'treadmill', 'dvd', 'vanish', 'epilepsy', 'matrix'}
-        end,
-        nargs="?"
-    }
+    complete = function()
+        return { 'treadmill', 'dvd', 'vanish', 'epilepsy', 'matrix' }
+    end,
+    nargs = "?"
+}
 )

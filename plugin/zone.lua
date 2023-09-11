0ddf96cad2fa369808203f6ddab5c4c32fba86c2
Pat <pkindsmuller@hotmail.de>
2023-09-12 01:36:01 +0200

    added support for custom command

--- plugin/zone.lua
+++ plugin/zone.lua
@@ -6,7 +6,7 @@ vim.api.nvim_create_user_command('Zone', function(opt)
     require("zone.styles." .. style).start()
 end, {
     complete = function()
-        return { 'treadmill', 'dvd', 'vanish', 'epilepsy', 'matrix' }
+        return { 'treadmill', 'dvd', 'vanish', 'epilepsy', 'matrix', 'customcmd' }
     end,
     nargs = "?"
 }
